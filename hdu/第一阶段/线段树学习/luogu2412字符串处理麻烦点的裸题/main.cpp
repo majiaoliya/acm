00b86746726a0d664f8d093be2b8935d3bc8ce57
majiao <majiao@qq.com>
2019-12-04 20:51:57 +0800

    字符串映射有点繁琐的线段树入门题

--- hdu/第一阶段/线段树学习/luogu2412字符串处理麻烦点的裸题/main.cpp
+++ hdu/第一阶段/线段树学习/luogu2412字符串处理麻烦点的裸题/main.cpp
@@ -9,69 +9,59 @@
 #include <algorithm>
 #include <vector>
 #include <string.h>
-#define MAXN 100050
+#define MAXN ((int)5e4+7)
 #include <map>
 #include <unordered_set>
 #define ll long long int
-
 using namespace std;
 
 /**
- * luogu2253
- * 求最长连续交错01子串 copy的模板
+ * 题意:给定n个字符串,给定Q个查询,每次查询strs[L,R]里字典序最大的那个str
+ * 思路:先把字符串备份,排序,映射好下标
+ * 		再建线段树 裸模板直接套就行了
  */
 
-int n, m;
+
+#define cmpi (i<(int)str.length() && k<(int)y.str.length())
+#define CHAR(x) (x>='A'&&x<='Z' ? x+('a'-'A') : x)
+
+int n, m, arr[MAXN], tree[MAXN<<2];
 struct Node {
-	int L, R, Mid;
-	int Lf, Rf, len;
-} Tree[MAXN<<2];
+	int id; //str在原数组中的位置
+	string str;
+	bool operator < (const Node& y) const { 
+		int i = 0, k = 0;
+		for( ; cmpi; i++, k++) { //比较字符串要忽略大小写
+			char cha = CHAR(str[i]), chb = CHAR(y.str[k]);
+			if(cha == chb) continue ;
+			return cha < chb;
+		}
+		return str.length() < y.str.length();
+	}
+} nodes[MAXN];
 
-inline void pushup(int rt)
-{
-    Tree[rt].L=Tree[rt<<1].L;
-    Tree[rt].R=Tree[rt<<1|1].R;
-    Tree[rt].Lf=Tree[rt<<1].Lf;
-    Tree[rt].Rf=Tree[rt<<1|1].Rf;
-    Tree[rt].Mid=max(Tree[rt<<1].R,Tree[rt<<1].Mid);
-    Tree[rt].Mid=max(Tree[rt].Mid,Tree[rt<<1|1].L);
-    Tree[rt].Mid=max(Tree[rt].Mid,Tree[rt<<1|1].Mid);
-    if(Tree[rt<<1].Rf!=Tree[rt<<1|1].Lf)
-    {
-        Tree[rt].Mid=max(Tree[rt<<1].R+Tree[rt<<1|1].L,Tree[rt].Mid);
-        if(Tree[rt<<1].L==Tree[rt<<1].len)
-            Tree[rt].L+=Tree[rt<<1|1].L;
-        if(Tree[rt<<1|1].R==Tree[rt<<1|1].len)
-            Tree[rt].R+=Tree[rt<<1].R;
-    }
-    return;
-}
+string s[MAXN]; //原字符串数组
 
-void Build(int rt,int l,int r)
-{
-    Tree[rt].len=r-l+1;
-    if(l==r)
-    {
-        Tree[rt].L=Tree[rt].R=Tree[rt].Mid=1;
-        Tree[rt].Lf=Tree[rt].Rf=0;
-        return;
-    }
-    int m=(l+r)>>1;
-    Build(rt<<1,l,m);
-    Build(rt<<1|1,m+1,r);
-    pushup(rt);
-    return;
-}
+inline void push_up(int rt) { tree[rt] = max(tree[rt<<1], tree[rt<<1|1]); }
 
-void update(int rt, int lef, int rig, int k) {
+void build(int rt, int lef, int rig) {
 	if(lef == rig) {
-		Tree[rt].Lf = Tree[rt].Rf = (Tree[rt].Rf+1) % 2;
+		tree[rt] = arr[lef];
 		return ;
 	}
 	int mid = (lef + rig) >> 1;
-	if(k <= mid) update(rt<<1, lef, mid, k);
-	else update(rt<<1|1, mid+1, rig, k);
-	pushup(rt);
+	build(rt<<1, lef, mid);
+	build(rt<<1|1, mid+1, rig);
+	push_up(rt);
+}
+
+int L, R;
+int query(int rt, int lef, int rig) {
+	if(L<=lef && rig<=R) return tree[rt];
+	int mid = (lef + rig) >> 1, ret = 0;
+	if(L <= mid) ret = query(rt<<1, lef, mid);
+	if(mid < R) ret = max(ret, query(rt<<1|1, mid+1, rig));
+	return ret;
 }
 
 int main() {
@@ -79,15 +69,21 @@ int main() {
 	freopen("test", "r", stdin);
 	clock_t stime = clock();
 #endif
-
-	int N, M, i, x;
-	scanf("%d %d ", &N, &M);
-	Build(1, 1, N);
-	for(int i=1; i<=M; i++) {
-		scanf("%d ", &x);
-		update(1, 1, N, x);
-		printf("%d\n", Tree[1].Mid);
+	ios::sync_with_stdio(false);
+	cin >> n >> m;
+	for(int i=1; i<=n; i++) {
+		cin >> nodes[i].str;
+		s[i] = nodes[i].str;
+		nodes[i].id = i;
 	}
+	sort(nodes+1, nodes+1+n);
+	for(int i=1; i<=n; i++) arr[nodes[i].id] = i;
+	build(1, 1, n);
+	for(int i=1; i<=m; i++) {
+		cin >> L >> R;
+		cout << s[nodes[query(1, 1, n)].id] << endl;
+	}
+	
 
 
 
